bc4e545d38dd8d0bfab43b36865a7152f85fc4fe
91song4 <90318817+91song4@users.noreply.github.com>
2023-07-31 09:20:05 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 0.49 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.js
@@ -1,57 +1,17 @@
-function 만료일구하기(term, privacy) {
-  privacy = privacy.split('.');
-
-  privacy[1] = +privacy[1] + term;
-
-  while (privacy[1] > 12) {
-    privacy[1] -= 12;
-    +privacy[0]++;
-  }
-
-  if (privacy[2] === '01') {
-    privacy[2] = '28';
-    if (privacy[1] === 1) {
-      privacy[1] = '12';
-      privacy[0] = String(+privacy[0] - 1);
-    } else {
-      privacy[1] = String(+privacy[1] - 1);
-    }
-  } else {
-    privacy[2] = String(+privacy[2] - 1).padStart(2, 0);
-  }
-
-  privacy[0] += '';
-  privacy[1] = String(privacy[1]).padStart(2, 0);
-  return privacy.join('.');
-}
-
 function solution(today, terms, privacies) {
-  // 약관종류와 유효기간 미리 정의
-  const tempTerms = {};
-  terms.forEach((term) => {
-    tempTerms[term.split(' ')[0]] = +term.split(' ')[1];
+  var answer = [];
+  var [year, month, date] = today.split(".").map(Number);
+  var todates = year * 12 * 28 + month * 28 + date;
+  var t = {};
+  terms.forEach((e) => {
+    let [a, b] = e.split(" ");
+    t[a] = Number(b);
   });
-
-  let i = 1;
-
-  const answer = [];
-
-  privacies.forEach((privacyArr) => {
-    const privacy = privacyArr.split(' ')[0]; // 개인정보 수집 일자
-    const term = privacyArr.split(' ')[1]; // 계약서의 약관종류
-
-    const result = 만료일구하기(tempTerms[term], privacy);
-
-    if (result < today) {
-      answer.push(i);
-    }
-    i++;
+  privacies.forEach((e, i) => {
+    var [day, term] = e.split(" ");
+    day = day.split(".").map(Number);
+    var dates = day[0] * 12 * 28 + day[1] * 28 + day[2] + t[term] * 28;
+    if (dates <= todates) answer.push(i + 1);
   });
-  // 계약서의 약관종류를 파악 - OK
-  // 약관의 유효기간을 구한다. - OK
-  // 만료일을 구한다. - OK
-  // if 만료일>today then 파기 else 파기안함. - OK
-
-  console.log(answer);
   return answer;
 }
