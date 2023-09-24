7d60565ee2a5a5a05bd5acecfb9b318dd344b1c1
91song4 <90318817+91song4@users.noreply.github.com>
2023-09-24 11:28:18 +0900

    [unrated] Title: 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
+++ 프로그래머스/unrated/164671. 조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기/조회수가 가장 많은 중고거래 게시판의 첨부파일 조회하기.sql
@@ -1,14 +1,12 @@
 -- 코드를 입력하세요
 SELECT 
-    CONCAT('/home/grep/src/', 
-        CONCAT(BOARD_ID,
-            CONCAT('/',
-                CONCAT(FILE_ID, 
-                    CONCAT(FILE_NAME, FILE_EXT)
-                )
-            )
-        )
-    ) AS FILE_PATH
+    CONCAT(
+        '/home/grep/src/',
+        BOARD_ID,
+        '/',
+        FILE_ID,
+        FILE_NAME,
+        FILE_EXT) AS FILE_PATH
 FROM USED_GOODS_FILE
 NATURAL JOIN (SELECT
     BOARD_ID
@@ -16,3 +14,4 @@ FROM USED_GOODS_BOARD
 ORDER BY VIEWS DESC
 LIMIT 1) UB
 ORDER BY FILE_ID DESC;
+
